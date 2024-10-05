4acc32138ed48510301f116d2cda7a75b68309b9
Nitsan Avni <nitsanav@gmail.com>
2024-10-05 19:36:27 +0200

    - t clean up

--- test/test.sh
+++ test/test.sh
@@ -4,13 +4,8 @@ cd "$(dirname "$0")"
 
 source ../env/venv/bin/activate
 
-# Use tmux-story for generating the script
+rm -f test_python_repl_print*.received*
+
 ../src/tmux-story ./example_test.yml
 
 diff ./generated_example_test.sh ./generated_example_test.sh.approved
-
-chmod +x generated_example_test.sh
-
-rm -f test_python_repl_print*.received*
-
-./generated_example_test.sh
